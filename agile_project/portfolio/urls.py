42b4e3e8934b131034dd66cee35d521195aa90b4
Кирилл Смирнов <ksmirnov@fura.ru>
2022-04-12 20:29:56 +0300

    fix: проверить главную

--- agile_project/portfolio/urls.py
+++ agile_project/portfolio/urls.py
@@ -5,7 +5,7 @@ app_name = 'portfolio'
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', index),
+    path('', index, name="index"),
     path('login', login, name="login"),
     path('registration', registration, name="registration"),
 ]
